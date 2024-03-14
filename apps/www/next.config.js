8afbd40bcb666cba1b70fdbc70fd74382a4d6784
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-03-13 21:22:02 -0300

    chore: initialise the `apps/www` website app

---
+++ apps/www/next.config.js
@@ -0,0 +1,10 @@
+const defaultConfiguration = require('@louffee/next-config/next.config')
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const configuration = {
+  ...defaultConfiguration,
+}
+
+module.exports = configuration
