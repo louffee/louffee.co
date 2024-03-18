d36ea586688c05266dc929906dfa1dbb94fdab35
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-03-17 22:32:04 -0300

    build: instruct `next.config` to ignore the `pg-native` and `cloudflare`
    This commit is motivated by the issue occurring when the Postgres client
    `pg` cannot be read by the Next.js server. See the thread on GitHub
    discussions for the proper context on the matter:
    https://github.com/vercel/next.js/discussions/50177#discussioncomment-5972779

--- config/next-config/next.config.js
+++ config/next-config/next.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@louffee/ui', '@louffee/lib'],
+  // Issue solved by ecumene on this discussion on vercel/next.js's GitHub page:
+  // https://github.com/vercel/next.js/discussions/50177#discussioncomment-5972779
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
+    )
+
+    return config
+  },
 }
